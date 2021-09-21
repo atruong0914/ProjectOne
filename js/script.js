5e252d1639464a7082f481a1d4b8eb32fe3f2229
annika truong <annikatruongt@gmail.com>
2021-09-21 09:04:15 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $('#btn-continue').click(function(){
     window.location.href='page1.html';
  })
+
+ $('#btn-yes').click(function(){
+    window.location.href='yespage.html';
+ })
+
+ $('#btn-back').click(function(){
+    window.location.href='index.html';
+ })
+
+ $('#btn-back1').click(function(){
+    window.location.href='page1.html';
+ })
+
+ $('#btn-no').click(function(){
+    window.location.href='nopage.html';
+ })
